b59d0374a685a27a440614efff081b5afa8cde9a
Eum3l <emil.krapf@gmail.com>
2021-11-02 10:51:17 +0100

    first test

--- cogs/ReactionRole.py
+++ cogs/ReactionRole.py
@@ -42,6 +42,7 @@ class ReactionRole(commands.Cog):
 
     @commands.Cog.listener(name="on_reaction_add")
     async def on_reaction_add(self, reaction, user):
+        await reaction.message.channel.send("Debug: onreactionadd")
         if reaction.message.channel == self.channelid:
             for self.em in self.emojireturn():
                 if reaction.emoji == self.em:
