18d2e130afeaba6a35543621118e5445fafb6c62
Eum3l <emil.krapf@gmail.com>
2021-11-02 09:51:36 +0100

    first test

--- cogs/ReactionRole.py
+++ cogs/ReactionRole.py
@@ -4,10 +4,47 @@ from discord.ext import commands
 class ReactionRole(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.roles = []
 
 
+    def emojireturn(self):
+        self.emojis = []
+        for self.role in self.roles:
+            self.emojis.append(self.role[1])
+        return self.emojis
 
+    def rolereturn(self, emoji):
+        self.r = ""
+        for self.role in self.roles:
+            if self.role[0] == emoji:
+                self.r = self.role[1]
+        return self.r
 
+    @commands.command(name="roinit")
+    async def roinit(self, ctx, arg):
+        self.channel = ctx.channel
+        self.channelid = ctx.channel.id
+        self.text = arg
+
+    @commands.command(name="roadd")
+    async def roadd(self, ctx, arg):
+        self.args = arg.split(" ")
+        self.roles.append(self.args)
+
+    @commands.command(name="rodeploy")
+    async def rodeploy(self):
+        self.msg = self.channel.send(self.text)
+        for self.emoji in self.emojireturn():
+            self.bot.add_reaction(self.msg, self.emoji)
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if reaction.message.channel == self.channelid:
+            for self.emoji in self.emojireturn():
+                if reaction.emoji == self.emoji:
+                    roget = discord.utils.get(user.server.roles, self.rolereturn(self.emoji))
+                    break
+            await self.bot.add_roles(user, roget)
 
 def setup(bot):
     bot.add_cog(ReactionRole(bot))
