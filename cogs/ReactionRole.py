baa54c635311768fb5ac7ac524c78ab4f215ca69
Eum3l <emil.krapf@gmail.com>
2021-11-05 16:58:50 +0100

    Persistend Role 2.0 - Test

--- cogs/ReactionRole.py
+++ cogs/ReactionRole.py
@@ -9,9 +9,12 @@ class ReactionRole(commands.Cog):
     def __init__(self, bot):
         with open("rorole.conf", "r") as f:
             self._data = f.readlines
-            self.channelid = self._data[0]
-            for self.r in range(len(1, len(self._data))):
-                self.roles.append(list(self._data[self.r]))
+            if self._data != [] or self._data != None:
+                self.channelid = self._data[0]
+                for self.r in range(len(1, len(self._data))):
+                    self.roles.append(list(self._data[self.r]))
+            else:
+                print("No Data loaded")
             f.close()
 
         self.bot = bot
