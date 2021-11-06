85e182510f4dd30f7503dbd745dae7e959020201
Eum3l <emil.krapf@gmail.com>
2021-11-06 09:50:16 +0100

    Cleanup

--- cogs/ReactionRole.py
+++ cogs/ReactionRole.py
@@ -39,7 +39,7 @@ class ReactionRole(commands.Cog):
             f.close()
 
     # returns the role for a emoji
-    def rolereturn(self, emoji):
+    def rolereturn(self, emoji, guild):
         for r in self.roles:
             if r[1] == emoji:
                 ro = r[0]
@@ -49,20 +49,14 @@ class ReactionRole(commands.Cog):
     @commands.command(name="roinit")
     async def roinit(self, ctx, arg):
         self.roles = []
-        self.channel = ctx.channel
         self.channelid = ctx.channel.id
         self.text = arg
-        try:
-            await self.msg.delete()
-        except:
-            print("First init this session")
         await ctx.send("Init Done!")
 
     # adds the roles to the list
     @commands.command(name="roadd")
     async def roadd(self, ctx, arg):
         self.args = arg.split(",")
-        self.args[1] = self.args[1].replace("\\", "")
         self.roles.append(self.args)
         await ctx.send(f"Add \"{self.args}\" ")
 
@@ -70,18 +64,17 @@ class ReactionRole(commands.Cog):
     @commands.command(name="rodeploy")
     async def rodeploy(self, ctx):
         self.config()
+        print(self.roles)
         await self.channel.purge(limit=len(await ctx.channel.history().flatten()))
-        self.msg = await self.channel.send(self.text)
-        for self.emoji in self.emojireturn():
-            await self.msg.add_reaction(self.emoji)
+        msg = await self.channel.send(self.text)
+        for emoji in self.emojireturn():
+            await msg.add_reaction(emoji)
 
     # reaction listeners
     @commands.Cog.listener(name="on_raw_reaction_add")
     async def on_raw_reaction_add(self, payload):
-        channel = self.bot.get_channel(payload.channel_id)
         guild = self.bot.get_guild(payload.guild_id)
         user = guild.get_member(payload.user_id)
-        message = await channel.fetch_message(payload.message_id)
 
         try:
             self.getconf(payload.channel_id)
@@ -91,15 +84,13 @@ class ReactionRole(commands.Cog):
         for em in self.emojireturn():
             if str(em) == str(payload.emoji):
                 if not user.bot:
-                    await user.add_roles(self.rolereturn(em))
+                    await user.add_roles(self.rolereturn(em, guild))
                 break
 
     @commands.Cog.listener(name="on_raw_reaction_remove")
     async def on_raw_reaction_remove(self, payload):
-        channel = self.bot.get_channel(payload.channel_id)
         guild = self.bot.get_guild(payload.guild_id)
         user = guild.get_member(payload.user_id)
-        message = await channel.fetch_message(payload.message_id)
 
         try:
             self.getconf(payload.channel_id)
@@ -109,7 +100,7 @@ class ReactionRole(commands.Cog):
         for em in self.emojireturn():
             if str(em) == str(payload.emoji):
                 if not user.bot:
-                    await user.remove_roles(self.rolereturn(em))
+                    await user.remove_roles(self.rolereturn(em, guild))
                 break
 
 
