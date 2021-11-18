b0ace5a29506c5112e393ba834225347aed2b774
blockcrafter21 <72599189+blockcrafter21@users.noreply.github.com>
2021-11-18 14:46:54 +0100

    add bug command

--- cogs/sound.py
+++ cogs/sound.py
@@ -21,6 +21,7 @@ class sound(commands.Cog):
             self.vc = await self.voice_channel.connect()
             self.vc.play(discord.FFmpegPCMAudio(source="music/earrape.mp3"))
             while self.vc.is_playing():
+                await asyncio.sleep(0.5)
             try:
                 await self.vc.disconnect()
             except:
