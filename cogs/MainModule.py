a790f242fcbe863664ef5eb335580255b0c27d03
blockcrafter21 <72599189+blockcrafter21@users.noreply.github.com>
2021-11-18 15:07:00 +0100

    update tts command

--- cogs/MainModule.py
+++ cogs/MainModule.py
@@ -55,29 +55,16 @@ class MainModule(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        """The event triggered when an error is raised while invoking a command.
-        Parameters
-        ------------
-        ctx: commands.Context
-            The context used for command invocation.
-        error: commands.CommandError
-            The Exception raised.
-        """
 
-        # This prevents any commands with local handlers being handled here in on_command_error.
         if hasattr(ctx.command, 'on_error'):
             return
 
-        # This prevents any cogs with an overwritten cog_command_error being handled here.
         cog = ctx.cog
         if cog:
             if cog._get_overridden_method(cog.cog_command_error) is not None:
                 return
 
         ignored = ()
-
-        # Allows us to check for original exceptions raised and sent to CommandInvokeError.
-        # If nothing is found. We keep the exception passed to on_command_error.
         error = getattr(error, 'original', error)
 
         # Anything in ignored will return and prevent anything happening.
@@ -96,10 +83,12 @@ class MainModule(commands.Cog):
             except discord.HTTPException:
                 pass
 
-        # For this error example we check to see where it came from...
         elif isinstance(error, commands.BadArgument):
-            if ctx.command.qualified_name == 'tag list':  # Check if the command being invoked is 'tag list'
+            if ctx.command.qualified_name == 'tag list':
                 await ctx.send('I could not find that member. Please try again.')
+
+        elif isinstance(error, commands.MissingRequiredArgument()):
+            await ctx.send(f"You forgot an argument")
 
         elif isinstance(error, discord.errors.NotFound):
             print("some404 error")
