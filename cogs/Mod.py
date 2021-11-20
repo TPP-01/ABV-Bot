86560ed559486dc3cbdfd93c7ca35040cbed1a71
blockcrafter21 <72599189+blockcrafter21@users.noreply.github.com>
2021-11-20 15:25:57 +0100

    add suggestion command in Utility.py

---
+++ cogs/Mod.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+
+
+class Mod(commands.Cog):
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(Mod(bot))
--- cogs/Utility.py
+++ cogs/Utility.py
@@ -5,6 +5,8 @@ import secrets
 from github import Github
 import platform
 import psutil
+
+
 class utility(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -42,12 +44,35 @@ class utility(commands.Cog):
         embed.add_field(name="Title", value=issue_title, inline=False)
         embed.add_field(name="Body", value=issue_body, inline=True)
         await ctx.send(embed=embed)
+    @commands.command(name="suggest", aliases=["sug", "idea"])
+    async def suggest(self, ctx):
+        g = Github(secrets.PAT_GH)
+        await ctx.send(
+            "Sende nun bitte den Titel bzw eine Kurzbeschreibung des Features das du vorschlagen möchtest/Now please send the Title or a very very short description of your feature that you want to suggest ")
+
+        def check(message):
+            return message.author == ctx.author and message.channel == ctx.channel
+
+        message = await self.bot.wait_for("message", check=check)
+        issue_title = message.content
+        await ctx.send(
+            "Sende nun bitte die ausführliche Beschreibung mit genauen Details/Now please send your feature and a detailed description of it ")
+        message = await self.bot.wait_for("message", check=check)
+        issue_body = message.content
+        repo = g.get_repo("TPP-01/ABV-Bot")
+        label = repo.get_label("suggestion_bot ")
+        repo.create_issue(title=issue_title, body=issue_body, labels=[label])
+        embed = discord.Embed(title="Suggestion Issue was created", color=0xff0000)
+        embed.add_field(name="Title", value=issue_title, inline=False)
+        embed.add_field(name="Body", value=issue_body, inline=True)
+        await ctx.send(embed=embed)
+
 
     @commands.command(name="info",aliases=["bot"])
     async def info(self, ctx):
         cpufreq = psutil.cpu_freq()
         embed = discord.Embed(title="Sys Info", color=0x0000f0)
-        embed.add_field(name="Cores(Logical and Physical", value=psutil.cpu_count(logical=True), inline=True)
+        embed.add_field(name="Cores(Logical and Physical)", value=psutil.cpu_count(logical=True), inline=True)
         embed.add_field(name="Cpu Type", value=platform.processor(), inline=True)
         embed.add_field(name="CPU Freq", value=f"{cpufreq.current:.2f}Mhz", inline=True)
         embed.add_field(name="CPU Usage", value=f"{psutil.cpu_percent()}%", inline=True)
