8e928be8e5436c64ed1892ce0163e3b8f0cb6c80
blockcrafter21 <72599189+blockcrafter21@users.noreply.github.com>
2021-11-25 14:17:54 +0100

    add ticket module

---
+++ cogs/Ticket.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+
+class Ticket(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+
+def setup(bot):
+    bot.add_cog(Ticket(bot))
