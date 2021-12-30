5e1c1d53f3bd34cb7c6189b63fea449daa8b37a8
blockcrafter21 <72599189+blockcrafter21@users.noreply.github.com>
2021-12-30 13:44:21 +0100

    update minecraft cog

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -2,8 +2,8 @@ from discord.ext import commands
 import discord
 import json
 import requests
-
-
+import random
+# Credits to the API to https://github.com/Iapetus-11
 class minecraft(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -11,25 +11,40 @@ class minecraft(commands.Cog):
     @commands.command(name="serverinfo", aliases=["si"])
     async def serverinfo(self, ctx, ip):
         player_list_cut = []
-        API_URL = f"https://api.mcsrvstat.us/2/{str(ip)}"
+        API_URL = f"https://api.iapetus11.me/mc/status/{str(ip)}"
         r = requests.get(API_URL)
         response = r.json()
         res = response
-        print(response["debug"]["apiversion"])
-        IMG_API = f"https://api.iapetus11.me/mc/servercard/{str(ip)}"
-        #img_r = requests.get(IMG_API)
-        if res["debug"]["ping"]:
-            online_players = res["players"]["online"]
-            max_players = res["players"]["max"]
-            player_list = res["players"]["list"]
+        backtick = "`"
+        seperator = ","
+        players = ""
+        #IMG_API = f"https://api.iapetus11.me/mc/servercard/{str(ip)}"
+        if res["online"]:
+            online_players = res["players_online"]
+            max_players = res["players_max"]
+            player_list = res["players"]
             for p in player_list:
-                if not ("§" in p or len(p) > 16 or len(p) < 3 or " " in p or "-" in p):
-                    player_list_cut.append(p)
+                name = p["name"]
+                player_list_cut.append(name)
+                #print(player_list)[debug]
+                #print(player_list_cut)[debug]
+            for name in player_list_cut:
+                if player_list_cut.index(name) == len(player_list_cut) - 1:
+                    players = players + backtick + name + backtick
+
+                else:
+                    players = players + backtick + name + backtick + seperator            # add \n to make new line for every player
             embed = discord.Embed(title=f"{ip} is online", color=0x07e43e)
-            embed.set_image(url=IMG_API)
+            embed.set_image(url=f"https://api.iapetus11.me/mc/servercard/{str(ip)}?v={random.random()*100000}")
             embed.add_field(name="Ip", value=ip, inline=True)
-            embed.add_field(name="Version", value=res["version"], inline=True)
-            embed.add_field(name=f"Online Players ({online_players}/{max_players}", value=player_list_cut, inline=False)
+            embed.add_field(name="Version", value=res["version"]["software"], inline=True)
+            embed.add_field(name=f"Online Players ({online_players}/{max_players}", value=players, inline=False)
+            embed.set_footer(text="made by the ABV-Bot Development Team, (inspired by https://github.com/Iapetus-11 `s Villager-Bot)")
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(title=f"No Minecraft Server detected at: {str(ip)} is down", color=0xff0000)
+            embed.set_footer(
+                text="made by the ABV-Bot Development Team, (inspired by https://github.com/Iapetus-11 `s Villager-Bot)")
             await ctx.send(embed=embed)
 
 
