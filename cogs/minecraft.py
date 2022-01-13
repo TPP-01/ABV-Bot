893d151896db963409e301d80ceae566bd410fcb
blockcrafter21 <72599189+blockcrafter21@users.noreply.github.com>
2022-01-13 16:26:43 +0100

    update minecraft.py NOT WORKING

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -5,7 +5,7 @@ import discord
 import requests
 import random
 import asyncio
-import cust
+import custom_modules.mcserv_functs
 # Credits to the API to https://github.com/Iapetus-11
 class minecraft(commands.Cog):
 
@@ -17,50 +17,13 @@ class minecraft(commands.Cog):
                 data = json.load(f)
                 return data
         self.lock = asyncio.Lock()
-        self.minecraft_server_checker.start()
+        self.mysticcraft_serv_checker.start()
         self.json_data = json_para()
 
 
 
     @tasks.loop(seconds=5.0)
     async def minecraft_server_checker(self):
-        def server_info_funct(self, ip):
-            player_list_cut = []
-            API_URL = f"https://api.iapetus11.me/mc/status/{str(ip)}"
-            r = requests.get(API_URL)
-            response = r.json()
-            res = response
-            backtick = "`"
-            seperator = ","
-            players = ""
-            # IMG_API = f"https://api.iapetus11.me/mc/servercard/{str(ip)}"
-            if res["online"]:
-                online_players = res["players_online"]
-                max_players = res["players_max"]
-                player_list = res["players"]
-                for p in player_list:
-                    name = p["name"]
-                    player_list_cut.append(name)
-                    # print(player_list)[debug]
-                    # print(player_list_cut)[debug]
-                for name in player_list_cut:
-                    if player_list_cut.index(name) == len(player_list_cut) - 1:
-                        players = players + backtick + name + backtick
-
-                    else:
-                        players = players + backtick + name + backtick + seperator  # add \n to make new line for every player
-                embed = discord.Embed(title=f"{ip} is online", color=0x07e43e)
-                embed.set_image(url=f"https://api.iapetus11.me/mc/servercard/{str(ip)}?v={random.random() * 100000}")
-                embed.add_field(name="Ip", value=ip, inline=True)
-                embed.add_field(name="Version", value=res["version"]["software"], inline=True)
-                embed.add_field(name=f"Online Players ({online_players}/{max_players}", value=players, inline=False)
-                embed.set_footer(
-                    text="made by the ABV-Bot Development Team, (inspired by https://github.com/Iapetus-11 `s Villager-Bot)")
-                return embed
-            else:
-                embed = discord.Embed(title=f"No Minecraft Server detected at: {str(ip)} !", color=0xff0000)
-                return embed
-
         print(str(self.json_data))
         data = self.json_data
         Servers = data.get("Servers")
@@ -70,9 +33,19 @@ class minecraft(commands.Cog):
             server_ip = i.get("ip")
             channel = discord.get_channel(channel_id)
             message = channel.fetch_message(message_id)
-            embed1 = server_info_funct(server_ip)
-            await message.edit(content=embed1)
+            embed1 = custom_modules.mcserv_functs.mcsrv_functs.server_info_funct(server_ip)
+            await message.edit(embed=embed1)
             print("done")
+    @tasks.loop(seconds=5.0)
+    async def mysticcraft_serv_checker(self):
+
+        server_ip_my = "185.208.205.128:25565"
+        guild_id_my = 907991840408608768
+        message_id_my = 930859497755377704
+        ch_id_my = 930547160376827914
+        channel = self.bot.get_channel(ch_id_my)
+        message = await channel.fetch_message(message_id_my)
+        await message.edit(content="test2")
 
 
 
