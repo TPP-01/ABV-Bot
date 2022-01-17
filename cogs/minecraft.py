b45a1287f17cacdd22b05d2270b9d415d4721f02
blockcrafter21 <72599189+blockcrafter21@users.noreply.github.com>
2022-01-17 14:25:58 +0100

    update minecraft.py

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -63,42 +63,8 @@ class minecraft(commands.Cog):
         await self.bot.wait_until_ready()
     @commands.command(name="serverinfo", aliases=["si"])
     async def serverinfo(self, ctx, ip):
-        player_list_cut = []
-        API_URL = f"https://api.iapetus11.me/mc/status/{str(ip)}"
-        r = requests.get(API_URL)
-        response = r.json()
-        res = response
-        backtick = "`"
-        seperator = ","
-        players = ""
-        #IMG_API = f"https://api.iapetus11.me/mc/servercard/{str(ip)}"
-        if res["online"]:
-            online_players = res["players_online"]
-            max_players = res["players_max"]
-            player_list = res["players"]
-            for p in player_list:
-                name = p["name"]
-                player_list_cut.append(name)
-                #print(player_list)[debug]
-                #print(player_list_cut)[debug]
-            for name in player_list_cut:
-                if player_list_cut.index(name) == len(player_list_cut) - 1:
-                    players = players + backtick + name + backtick
-
-                else:
-                    players = players + backtick + name + backtick + seperator            # add \n to make new line for every player
-            embed = discord.Embed(title=f"{ip} is online", color=0x07e43e)
-            embed.set_image(url=f"https://api.iapetus11.me/mc/servercard/{str(ip)}?v={random.random()*100000}")
-            embed.add_field(name="Ip", value=ip, inline=True)
-            embed.add_field(name="Version", value=res["version"]["software"], inline=True)
-            embed.add_field(name=f"Online Players ({online_players}/{max_players}", value=players, inline=False)
-            embed.set_footer(text="made by the ABV-Bot Development Team, (inspired by https://github.com/Iapetus-11 `s Villager-Bot)")
-            await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(title=f"No Minecraft Server detected at: {str(ip)} !", color=0xff0000)
-            embed.set_footer(
-                text="made by the ABV-Bot Development Team, (inspired by https://github.com/Iapetus-11 `s Villager-Bot)")
-            await ctx.send(embed=embed)
+        embed = custom_modules.mcserv_functs.mcsrv_functs.server_info_funct(ip=ip)
+        await ctx.send(embed=embed)
 
 
 
