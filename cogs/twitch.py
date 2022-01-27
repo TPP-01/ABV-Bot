3b1039083967ca6708ef24f996d5e46619ba7394
Eum3l <emil.krapf@gmail.com>
2022-01-27 18:39:25 +0100

    cogs.twitch - ENDLICH XD

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -15,7 +15,7 @@ class twitch(commands.Cog):
         ret = json.loads(requests.get(f'https://api.twitch.tv/helix/streams?user_login="{login_name}"',
                                       headers={"Authorization": f"Bearer {self.twitch_authorization_key}",
                                                "Client-Id": f"{self.twitch_client_id}"}).content.decode())
-
+        print(ret)
         streams, gamename, since = ret["data"] != [], ret["data"][0]["game_name"], \
                                    ret["data"][0]["started_at"].replace("Z", "").split("T")[1]
 
