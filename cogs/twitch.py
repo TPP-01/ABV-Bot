0d1d3feab818e55d99fdc1f6b1db0c53e4733991
Eum3l <emil.krapf@gmail.com>
2022-01-27 19:11:14 +0100

    cogs.twitch

---
+++ cogs/twitch.py
@@ -0,0 +1,37 @@
+import json
+
+import requests
+from discord.ext import commands
+
+import secrets
+
+class twitch(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.twitch_client_id = secrets.twitch_client_id
+        self.twitch_authorization_key = secrets.twitch_authorization_key
+
+    def doesstream(self, login_name):
+        ret = json.loads(requests.get(f'https://api.twitch.tv/helix/streams?user_login={login_name}',
+                                      headers={"Authorization": f"Bearer {self.twitch_authorization_key}",
+                                               "Client-Id": f"{self.twitch_client_id}"}).content.decode())
+
+        streams = ret["data"] != []
+        if streams:
+            gamename, since = ret["data"][0]["game_name"], ret["data"][0]["started_at"].replace("Z", "").split("T")[1]
+        else:
+            gamename, since = None, None
+
+        return streams, gamename, since
+
+    @commands.command(name="streams", help="Show the streaming state with login_names")
+    async def streams(self, ctx, login):
+        streams, gamename, since = self.doesstream(login)
+        if streams:
+            await ctx.send(f"{login} streamt {gamename} seit {since}!")
+        else:
+            await ctx.send(f"{login} streamt nicht.")
+
+
+def setup(bot):
+    bot.add_cog(twitch(bot))
