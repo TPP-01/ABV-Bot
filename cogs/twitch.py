3cea05db1d1889d0c30ec74dbb80fe064f834edf
Eum3l <emil.krapf@gmail.com>
2022-01-27 18:56:52 +0100

    cogs.twitch - gut

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -14,20 +14,22 @@ class twitch(commands.Cog):
         ret = json.loads(requests.get(f'https://api.twitch.tv/helix/streams?user_login={login_name}',
                                       headers={"Authorization": f"Bearer {self.twitch_authorization_key}",
                                                "Client-Id": f"{self.twitch_client_id}"}).content.decode())
-        try:
-            streams, gamename, since = ret["data"] != [], ret["data"][0]["game_name"], \
-                                       ret["data"][0]["started_at"].replace("Z", "").split("T")[1]
-        except IndexError:
-            streams, gamename, since = False, None, None
+
+        streams = ret["data"] != []
+        if streams:
+            gamename, since = ret["data"][0]["game_name"], ret["data"][0]["started_at"].replace("Z", "").split("T")[1]
+        else:
+            gamename, since = None, None
+
         return streams, gamename, since
 
     @commands.command(name="streams", help="Show the streaming state with login_names")
     async def streams(self, ctx, login):
         streams, gamename, since = self.doesstream(login)
         if streams:
-            await ctx.send(f"{login} streamt {gamename} seit {since}")
+            await ctx.send(f"{login} streamt {gamename} seit {since}!")
         else:
-            await ctx.send(f"{login} streamt nicht")
+            await ctx.send(f"{login} streamt nicht.")
 
 
 def setup(bot):
