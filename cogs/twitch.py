e95825c3d466443d053412e16b6bbddca3faedb4
Eum3l <emil.krapf@gmail.com>
2022-01-28 19:01:02 +0100

    cogs.twitch - reminder

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -1,9 +1,9 @@
 import json
+import secrets
 
 import requests
-from discord.ext import commands
+from discord.ext import commands, tasks
 
-import secrets
 
 class twitch(commands.Cog):
     def __init__(self, bot):
@@ -31,9 +31,27 @@ class twitch(commands.Cog):
             await ctx.send(f"{login} streamt {gamename} seit {since}!")
         else:
             await ctx.send(f"{login} streamt nicht.")
-        await ctx.delete()
+        if ctx.guild:
+            await ctx.message.delete()
+
+    @commands.command(name="remind", help="Remind when specific twitch-streamers stream.")
+    async def remind(self, ctx, login):
+        with open("../twitch.json", "r+") as j:
+            twitchjson = json.loads(j.read())
+            twitchjson[ctx.user.id] = twitchjson[ctx.user.id].append(login)
+            j.write(json.dumps(twitchjson))
+
+    @tasks.loop(seconds=120)
+    async def twitchreminder(self):
+        with open("../twitch.json", "r") as j:
+            twitchjson = json.loads(j.read())
+            for userid, streamers in twitchjson.items():
+                user = self.bot.get_user(userid)
+                for streamer in streamers:
+                    streams, gamename, since = self.doesstream(streamer)
+                    if streams == True:
+                        user.send(f"Der Streamer {streamer} streamt das Spiel {gamename}!")
 
 
 def setup(bot):
     bot.add_cog(twitch(bot))
-#
