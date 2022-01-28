60f6eb50df1a952c9d05b7043f5f7a2d22825032
Eum3l <emil.krapf@gmail.com>
2022-01-28 20:57:59 +0100

    cogs.twitch - reminder

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -79,6 +79,7 @@ class twitch(commands.Cog):
                         for streamer in streamers:
                             print(streamers)  ###################
                             streams, gamename, since = self.doesstream(streamer)
+                            print(streams)
                             if streams == True and int(str(datetime.datetime.now()).split(":")[1].split(".")[0]) - int(since.split(":")[2]) <= 3:
                                 print("Doesstream")  ###################
                                 await user.send(f"Der Streamer {streamer} streamt das Spiel {gamename}!")
