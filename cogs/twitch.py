762c5fbbbe3e84fa7ce66a45132dcc1c57be5038
Eum3l <emil.krapf@gmail.com>
2022-01-28 19:17:53 +0100

    cogs.twitch - reminder

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -56,6 +56,7 @@ class twitch(commands.Cog):
     async def twitchreminder(self):
         with open("twitch.json", "r") as j:
             try:
+                print(j.read())
                 twitchjson = json.loads(j.read())
                 print(twitchjson)###################
                 for userid, streamers in twitchjson.items():
