3ec8fd2df29626ec40503b3417768659363d6857
oa1 <72599189+oa01@users.noreply.github.com>
2024-07-18 14:32:08 +0200

    chore(requirements)

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_prefix(bot, message):
 
 
 #cogs to use
-initial_extensions = ["cogs.MainModule", "cogs.Fun", "cogs.BotOwner", "cogs.Admin", "cogs.minecraft", "cogs.sound", "cogs.memes", "cogs.Wetter"]
+initial_extensions = ["cogs.MainModule", "cogs.Fun", "cogs.BotOwner", "cogs.Admin", "cogs.minecraft", "cogs.sound", "cogs.memes"]
 bot = commands.Bot(command_prefix=get_prefix, description="The official ABV bot", intents=intents, help_command=None)
 
 if __name__ == '__main__':
